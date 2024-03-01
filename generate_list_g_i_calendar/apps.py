a4db71f347138b2d896a0c58942e700d8d61b2b4
ibroxim <ibrohimabdusalomov395@gmail.com>
2024-03-01 14:20:16 +0500

    commit

---
+++ generate_list_g_i_calendar/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GenerateListGICalendarConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "generate_list_g_i_calendar"
